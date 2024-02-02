3152ece5b3126cb1c9185199087bc405c727a480
islemjbli <158579551+islemjbli@users.noreply.github.com>
2024-02-02 14:47:40 +0100

    Add files via upload

---
+++ page.php
@@ -0,0 +1,17 @@
+<!doctype html >
+<html>
+  <head>
+    <title>Tp </title>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <?php
+    $message = "Bienvenue sur ma page PHP!";
+    echo $message;
+    $nom = "Islem";
+    $age = 25;
+    echo "Bonjour, je m'appelle $nom et j'ai $age ans.";
+    ?>
+ 
+  </body>
+</html>
